2911e06627b163d0d7257710cf591ecaeeaa08da
Peter Parente <parente@cs.unc.edu>
2013-12-31 10:12:48 -0500

    Force midori fullscreen

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
+cd `dirname $0`
 vendor/bin/python server.py &
 PID=$!
-python -m webbrowser -t "http://127.0.0.1:8080"
+midori -e Fullscreen -e TabCloseOther -a http://127.0.0.1:8080
 kill $PID
